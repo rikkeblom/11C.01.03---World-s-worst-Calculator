8e22bfbbd4630e216647a35a979bf469db08c56d
Rikke Blom <rikk957v@stud.kea.dk>
2021-08-29 13:08:55 +0200

    adjusting for decimals

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ console.log("Let's do math!");
 
 window.addEventListener("load", start);
 
+let result;
+
 function start() {
   console.log("function start()");
   document.querySelector("#calculate").addEventListener("click", calculate);
+  document.querySelector("#clear").addEventListener("click", clearResults);
 }
 
 function calculate() {
@@ -20,14 +23,38 @@ function calculate() {
   if (operator === "add") {
     console.log("add stuff");
     console.log(usefirstnumber + usesecondnumber);
+    result = usefirstnumber + usesecondnumber;
   } else if (operator === "sub") {
     console.log("subtract stuff");
     console.log(usefirstnumber - usesecondnumber);
+    result = usefirstnumber - usesecondnumber;
   } else if (operator === "mul") {
     console.log("multiply stuff");
     console.log(usefirstnumber * usesecondnumber);
+    result = usefirstnumber * usesecondnumber;
   } else if (operator === "div") {
     console.log("divide stuff");
     console.log(usefirstnumber / usesecondnumber);
+    result = usefirstnumber / usesecondnumber;
   }
+  adjustDecimals();
+}
+
+function adjustDecimals() {
+  console.log("adjustDecimals()");
+
+  if (document.querySelector("#doround:checked") !== null) {
+    console.log("round number please");
+    let decimaldropdown = document.querySelector("#decimals");
+    let decimalvalue = decimaldropdown.value;
+    console.log(decimalvalue);
+    result = result.toFixed(decimalvalue);
+    console.log(result);
+  } else {
+    console.log(result);
+  }
+}
+
+function clearResults() {
+  console.log("clear results");
 }
